e051a721ae13bf51b496556a673bc9e632d402b4
Lubnnia <lubnnia@gmail.com>
2023-08-14 21:01:46 -0300

    first commit

---
+++ src/env/index.ts
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+// Este comando safeParse vai tentar validar esse process.env para ver
+// se ele tem exatamente as variáveis mencionadas acima (NODE_ENV e PORT)
+
+if (_env.success === false) {
+  console.error('❌ Invalid environment variables', _env.error.format());
+
+  throw new Error('Invalid environment variables');
+}
+
+const env = _env.data;
+
+export { env };
