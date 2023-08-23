2de53e4109e95bc4ab46616f645ad1018af2ead5
Lubnnia <lubnnia@gmail.com>
2023-08-23 15:22:11 -0300

    feat: Create migration create_users

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,15 @@
 import fastify from 'fastify';
+import { PrismaClient } from '@prisma/client';
 
 const app = fastify();
+
+const prisma = new PrismaClient();
+
+prisma.user.create({
+  data: {
+    name: 'Lubnnia Morais',
+    email: 'lubnnia@gmail.com',
+  },
+});
 
 export { app };
