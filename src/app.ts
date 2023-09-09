57da629938ef6b405232dfd14cabd510af552343
Lubnnia <lubnnia@gmail.com>
2023-09-09 19:16:14 -0300

    feat: Create module errors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,27 @@
 import fastify from 'fastify';
 
 import { appRoutes } from './http/routes';
+import { ZodError } from 'zod';
+import { env } from './env';
 
 const app = fastify();
 
 app.register(appRoutes);
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    reply
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO: Here we should log to on external tool like DataDog/NewRelic/Sentry
+  }
+
+  reply.status(500).send({ message: 'Internal server error' });
+});
 
 export { app };
--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -4,6 +4,7 @@ import { z } from 'zod';
 
 import { RegisterUseCase } from '@/use-cases/register';
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository';
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error';
 
 async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -25,7 +26,11 @@ async function register(request: FastifyRequest, reply: FastifyReply) {
       password,
     });
   } catch (err) {
-    return reply.status(409).send();
+    if (err instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: err.message });
+    }
+
+    reply.status(500).send(); // TODO fix me
   }
 }
 
