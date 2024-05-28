4504c3dbef06954996372fe066db5834a3bf67b4
Lubnnia <lubnnia@gmail.com>
2024-05-28 09:40:25 -0300

    chore: Add refresh token controller

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -4,11 +4,14 @@ import { register } from './register';
 import { authenticate } from './authenticate';
 import { profile } from './profile';
 import { verifyJWT } from '../../middlewares/verify-jwt';
+import { refresh } from './refresh';
 
 export async function usersRoutes(app: FastifyInstance) {
   /** Unauthenticated routes */
   app.post('/users', register);
   app.post('/sessions', authenticate);
+
+  app.patch('/token/refresh', refresh);
 
   /** Authenticated routes */
   app.get('/me', { onRequest: [verifyJWT] }, profile);
